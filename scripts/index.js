d8ac8345da0734f3501d00f272e2653f49a934e1
zapatajuani <juaniLMC@gmail.com>
2024-05-22 02:10:38 -0300

    Cambio en el funcionamiento de los media querys

--- scripts/index.js
+++ scripts/index.js
@@ -45,12 +45,3 @@ if (!carrito) {
 
     })  
 }  
-
-/* ---------------------------------------------- */
-
-let icon = document.querySelector(".menu-icon")
-let navbar = document.querySelector(".navbar")
-
-icon.addEventListener("click", () => {    
-    navbar.classList.toggle("show")
-})
